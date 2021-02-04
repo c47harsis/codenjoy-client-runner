7367abffd47eb643ceea37b2969f1c812f85c06d
Valentin Chizhov <valentin.chizhov@gmail.com>
2021-02-04 07:45:55 +0300

    Docker transport switched from Jersey to Apache HttpClient 5 in accordance with official documentation. Jersey will be not longer supported in future releases of docker-java lib.

--- src/main/java/com/codenjoy/clientrunner/service/facade/DockerService.java
+++ src/main/java/com/codenjoy/clientrunner/service/facade/DockerService.java
@@ -7,7 +7,11 @@ import com.github.dockerjava.api.command.BuildImageResultCallback;
 import com.github.dockerjava.api.model.BuildResponseItem;
 import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +27,18 @@ public class DockerService {
 
     public static final String SERVER_PARAMETER = "CODENJOY_URL";
 
-    private final DockerClient docker = DockerClientBuilder.getInstance().build();
+    private final DockerClientConfig dockerClientConfig = DefaultDockerClientConfig
+            .createDefaultConfigBuilder()
+            .build();
+
+    private final DockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
+            .dockerHost(dockerClientConfig.getDockerHost())
+            .sslConfig(dockerClientConfig.getSSLConfig())
+            .build();
+
+    private final DockerClient docker = DockerClientBuilder.getInstance()
+            .withDockerHttpClient(dockerHttpClient)
+            .build();
 
     @PostConstruct
     protected void init() {
